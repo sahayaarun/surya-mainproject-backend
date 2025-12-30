412aa5fd392373f642dcb5a3e30cd3a775d9fd26
Surya <sahayaarun000@gmail.com>
2025-12-30 15:07:43 +0530

    fixed db connection to use env vaarible

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB;
